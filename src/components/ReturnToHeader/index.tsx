3fac8319e755b34219fd618007382367fabb1649
João Vitor Lima <jvolima2004@gmail.com>
2023-01-12 22:50:28 -0300

    feat: add scroll up animation

--- src/components/ReturnToHeader/index.tsx
+++ src/components/ReturnToHeader/index.tsx
@@ -1,10 +1,13 @@
 import { ArrowUp } from 'phosphor-react'
+import ScrollToTop from 'react-scroll-up'
 import { ReturnToHeaderContainer } from './styles'
 
 export function ReturnToHeader() {
   return (
-    <ReturnToHeaderContainer href="#header">
-      <ArrowUp size={28} color="#FFFFFF" weight="bold" />
-    </ReturnToHeaderContainer>
+    <ScrollToTop showUnder={100} duration={1000}>
+      <ReturnToHeaderContainer href="#header">
+        <ArrowUp size={28} color="#FFFFFF" weight="bold" />
+      </ReturnToHeaderContainer>
+    </ScrollToTop>
   )
 }
